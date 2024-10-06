c8379085240b4641316ea33bbe086b66dbc17350
Huzaif Shaikh <huzaifsk12@gmail.com>
2024-10-06 17:35:31 +0530

    resolving issue

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { createBrowserRouter } from "react-router-dom";
 
 import Layout from "./Layout";
@@ -6,7 +7,6 @@ import ErrorScreen from "./ErrorScreen";
 const router = createBrowserRouter([
   {
     path: "/",
-    exact: true,
     element: <Layout />,
     errorElement: <ErrorScreen />,
     children: [
