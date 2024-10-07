6b6419122c7ecf088bb2abf3e679b611cec00b2b
Huzaif Shaikh <huzaifsk12@gmail.com>
2024-10-07 22:07:30 +0530

    added navbar

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import Navbar from "./Navbar";
 
 function Layout() {
   return (
     <>
-      <main>
-        <Outlet />
+      <main className="flex flex-col">
+        <div className="">
+          <Navbar />
+        </div>
+        <div className="flex">
+          <Outlet />
+        </div>
       </main>
     </>
   );
