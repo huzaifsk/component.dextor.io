20385b54ab4fe4edefa94c80490bb2e0d5b3cc02
Huzaif Shaikh <huzaifsk12@gmail.com>
2024-10-10 20:22:19 +0530

    added tabler icon and removed svg

--- src/UiLivePreview.jsx
+++ src/UiLivePreview.jsx
@@ -179,10 +179,22 @@ export default function UiLivePreview() {
                     }}
                   >
                     <div className="flex align-center m-2">
-                      <img
-                        src="/public/copy.svg"
-                        className="w-10 h-10 mt-2 p-2"
-                      />
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="42"
+                        height="42"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        stroke="currentColor"
+                        stroke-width="1"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        className="icon icon-tabler icons-tabler-outline icon-tabler-copy mt-2 cursor-pointer"
+                      >
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                        <path d="M7 7m0 2.667a2.667 2.667 0 0 1 2.667 -2.667h8.666a2.667 2.667 0 0 1 2.667 2.667v8.666a2.667 2.667 0 0 1 -2.667 2.667h-8.666a2.667 2.667 0 0 1 -2.667 -2.667z" />
+                        <path d="M4.012 16.737a2.005 2.005 0 0 1 -1.012 -1.737v-10c0 -1.1 .9 -2 2 -2h10c.75 0 1.158 .385 1.5 1" />
+                      </svg>
                       <button className="mt-2 p-2 bg-blue-500 text-white rounded">
                         {copied ? "Code Copied!" : "Copy Code"}
                       </button>
