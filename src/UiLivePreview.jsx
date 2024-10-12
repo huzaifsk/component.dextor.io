2d1dff28cec6477c5c6bbe9c7e3a8d07c4691677
Huzaif Shaikh <huzaifsk12@gmail.com>
2024-10-13 00:50:00 +0530

    -> improved UI of live preview -> added mapping for fast rendering and optimizing -> created some components ->

--- src/UiLivePreview.jsx
+++ src/UiLivePreview.jsx
@@ -3,211 +3,115 @@ import { LiveProvider, LiveEditor, LivePreview, LiveError } from "react-live";
 import { CopyToClipboard } from "react-copy-to-clipboard";
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { ComponentData } from "./data/componentData";
 
 export default function UiLivePreview() {
-  const code = `
-  function Preview() {
-    const [isMenuOpen, setIsMenuOpen] = React.useState(false);
-    
-    const toggleMenu = () => {
-      setIsMenuOpen(!isMenuOpen);
-    };
-
-    return (
-      <nav className="block w-full max-w-screen-lg px-4 py-2 mx-auto bg-white shadow-md rounded-md lg:px-8 lg:py-3 mt-10">
-        <div className="container flex flex-wrap items-center justify-between mx-auto text-slate-800">
-          <a href="#" className="mr-4 block cursor-pointer py-1.5 text-base text-slate-800 font-semibold">
-            React Components
-          </a>
-          <div className={\`hidden lg:block\`}>
-            <ul className="flex flex-col gap-2 mt-2 mb-4 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
-              <li className="flex items-center p-1 text-sm gap-x-2 text-slate-600">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth="1.5"
-                  stroke="currentColor"
-                  className="h-6 w-6 text-slate-500"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 0 0 2.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 0 0-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75 2.25 2.25 0 0 0-.1-.664m-5.8 0A2.251 2.251 0 0 1 13.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25ZM6.75 12h.008v.008H6.75V12Zm0 3h.008v.008H6.75V15Zm0 3h.008v.008H6.75V18Z"
-                  />
-                </svg>
-
-                <a href="#" className="flex items-center">
-                  Pages
-                </a>
-              </li>
-              <li className="flex items-center p-1 text-sm gap-x-2 text-slate-600">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth="1.5"
-                  stroke="currentColor"
-                  className="h-6 w-6 text-slate-500"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
-                  />
-                </svg>
-
-                <a href="#" className="flex items-center">
-                  Account
-                </a>
-              </li>
-              <li className="flex items-center p-1 text-sm gap-x-2 text-slate-600">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth="1.5"
-                  stroke="currentColor"
-                  className="h-6 w-6 text-slate-500"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M2.25 7.125C2.25 6.504 2.754 6 3.375 6h6c.621 0 1.125.504 1.125 1.125v3.75c0 .621-.504 1.125-1.125 1.125h-6a1.125 1.125 0 0 1-1.125-1.125v-3.75ZM14.25 8.625c0-.621.504-1.125 1.125-1.125h5.25c.621 0 1.125.504 1.125 1.125v8.25c0 .621-.504 1.125-1.125 1.125h-5.25a1.125 1.125 0 0 1-1.125-1.125v-8.25ZM3.75 16.125c0-.621.504-1.125 1.125-1.125h5.25c.621 0 1.125.504 1.125 1.125v2.25c0 .621-.504 1.125-1.125 1.125h-5.25a1.125 1.125 0 0 1-1.125-1.125v-2.25Z"
-                  />
-                </svg>
-
-                <a href="#" className="flex items-center">
-                  Blocks
-                </a>
-              </li>
-              <li className="flex items-center p-1 text-sm gap-x-2 text-slate-600">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth="1.5"
-                  stroke="currentColor"
-                  className="h-6 w-6 text-slate-500"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M4.26 10.147a60.438 60.438 0 0 0-.491 6.347A48.62 48.62 0 0 1 12 20.904a48.62 48.62 0 0 1 8.232-4.41 60.46 60.46 0 0 0-.491-6.347m-15.482 0a50.636 50.636 0 0 0-2.658-.813A59.906 59.906 0 0 1 12 3.493a59.903 59.903 0 0 1 10.399 5.84c-.896.248-1.783.52-2.658.814m-15.482 0A50.717 50.717 0 0 1 12 13.489a50.702 50.702 0 0 1 7.74-3.342M6.75 15a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Zm0 0v-3.675A55.378 55.378 0 0 1 12 8.443m-7.007 11.55A5.981 5.981 0 0 0 6.75 15.75v-1.5"
-                  />
-                </svg>
-
-                <a href="#" className="flex items-center">
-                  Docs
-                </a>
-              </li>
-            </ul>
-          </div>
-          <button
-            className="relative ml-auto h-6 max-h-[40px] w-6 max-w-[40px] select-none rounded-lg text-center align-middle text-xs font-medium uppercase text-inherit transition-all hover:bg-transparent focus:bg-transparent active:bg-transparent disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none lg:hidden"
-            type="button"
-            onClick={toggleMenu}
-          >
-            <span className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
-              <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" fill="none" stroke="currentColor" strokeWidth="2">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M4 6h16M4 12h16M4 18h16"></path>
-              </svg>
-            </span>
-          </button>
-        </div>
-        <div className={\`\${isMenuOpen ? "block" : "hidden"} dropdown\`}>
-          <ul className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-full p-2 shadow">
-            <li><a>Homepage</a></li>
-            <li><a>Portfolio</a></li>
-            <li><a>About</a></li>
-          </ul>
-        </div>
-      </nav>
-    );
-  }
-  `;
-
   const [copied, setCopied] = useState(false);
-  const [activeTab, setActiveTab] = useState("preview");
 
   return (
     <>
       <div className="container mx-auto p-4">
-        <div className="border rounded-lg shadow-lg overflow-hidden">
-          {/* Tabs */}
-          <div className="flex border-b">
-            <button
-              className={`flex-1 p-2 text-center ${
-                activeTab === "preview" ? "bg-gray-200" : "hover:bg-gray-100"
-              }`}
-              onClick={() => setActiveTab("preview")}
-            >
-              UI Preview
-            </button>
-            <button
-              className={`flex-1 p-2 text-center ${
-                activeTab === "code" ? "bg-gray-200" : "hover:bg-gray-100"
-              }`}
-              onClick={() => setActiveTab("code")}
-            >
-              Code
-            </button>
-          </div>
+        {ComponentData &&
+          ComponentData.map((component) => {
+            const [activeTab, setActiveTab] = useState("preview");
 
-          <div className="m-5 font-medium text-black text-3xl">
-            <h2>Nav Bar</h2>
-          </div>
+            const toggleTab = (tab) => {
+              setActiveTab(tab);
+            };
 
-          {/* Content */}
-          <div className="p-4 flex justify-center">
-            {activeTab === "preview" ? (
-              <LiveProvider code={code}>
-                <div className="preview-wrapper">
-                  <LivePreview />
-                  <LiveError />
-                </div>
-              </LiveProvider>
-            ) : (
-              <div className="translate-y-0">
-                <LiveProvider code={code}>
-                  <CopyToClipboard
-                    text={code}
-                    onCopy={() => {
-                      setCopied(true);
-                      toast.success("Copied successfully");
-                      setTimeout(() => setCopied(false), 2000); // Reset copied state after 2 seconds
-                    }}
-                  >
-                    <div className="flex align-center m-2">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="42"
-                        height="42"
-                        viewBox="0 0 24 24"
-                        fill="none"
-                        stroke="currentColor"
-                        stroke-width="1"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        className="icon icon-tabler icons-tabler-outline icon-tabler-copy mt-2 cursor-pointer"
+            return (
+              <React.Fragment key={component.id}>
+                <div className="border rounded-lg shadow-lg overflow-hidden mt-5">
+                  <div className="m-5 font-medium text-black text-3xl">
+                    <h2>{component.title}</h2>
+                  </div>
+                  {/* Tabs */}
+                  <div className="w-full flex justify-center">
+                    <div className="flex w-2/3 ">
+                      <button
+                        className={`rounded-lg flex-1 py-4 px-6 text-sm font-medium transition duration-300 ease-in-out ${
+                          activeTab === "preview"
+                            ? "bg-blue-500 text-white"
+                            : "text-gray-600 hover:bg-gray-100"
+                        }`}
+                        onClick={() => toggleTab("preview")}
                       >
-                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                        <path d="M7 7m0 2.667a2.667 2.667 0 0 1 2.667 -2.667h8.666a2.667 2.667 0 0 1 2.667 2.667v8.666a2.667 2.667 0 0 1 -2.667 2.667h-8.666a2.667 2.667 0 0 1 -2.667 -2.667z" />
-                        <path d="M4.012 16.737a2.005 2.005 0 0 1 -1.012 -1.737v-10c0 -1.1 .9 -2 2 -2h10c.75 0 1.158 .385 1.5 1" />
-                      </svg>
-                      <button className="mt-2 p-2 bg-blue-500 text-white rounded">
-                        {copied ? "Code Copied!" : "Copy Code"}
+                        UI Preview
+                      </button>
+                      <button
+                        className={`rounded-lg flex-1 py-4 px-6 text-sm font-medium transition duration-300 ease-in-out ${
+                          activeTab === "code"
+                            ? "bg-blue-500 text-white"
+                            : "text-gray-600 hover:bg-gray-100"
+                        }`}
+                        onClick={() => toggleTab("code")}
+                      >
+                        Code
                       </button>
                     </div>
-                  </CopyToClipboard>
-                  <div className="max-h-72 min-h-10 overflow-y-auto rounded-lg">
-                    <LiveEditor className="font-mono border rounded-lg" />
                   </div>
-                </LiveProvider>
-              </div>
-            )}
-          </div>
-        </div>
+                  {/* Content */}
+                  <div className="p-6" key={component.id}>
+                    <LiveProvider code={component.code}>
+                      {activeTab === "preview" ? (
+                        <div className="preview-wrapper w-full bg-gray-50 rounded-lg shadow-inner p-6 flex justify-center">
+                          <LivePreview />
+                          <LiveError />
+                        </div>
+                      ) : (
+                        <div className="w-full">
+                          <CopyToClipboard
+                            text={component.code}
+                            onCopy={() => {
+                              setCopied(true);
+                              toast.dismiss();
+                              toast.success("Copied to clipboard");
+                              setTimeout(() => setCopied(false), 2000);
+                            }}
+                          >
+                            <div className="flex justify-end mb-4">
+                              <button className="flex items-center space-x-2 px-4 py-2 bg-blue-500 text-white text-sm font-medium rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition duration-300 ease-in-out">
+                                <svg
+                                  xmlns="http://www.w3.org/2000/svg"
+                                  width="16"
+                                  height="16"
+                                  viewBox="0 0 24 24"
+                                  fill="none"
+                                  stroke="currentColor"
+                                  strokeWidth="2"
+                                  strokeLinecap="round"
+                                  strokeLinejoin="round"
+                                  className="feather feather-copy"
+                                >
+                                  <rect
+                                    x="9"
+                                    y="9"
+                                    width="13"
+                                    height="13"
+                                    rx="2"
+                                    ry="2"
+                                  ></rect>
+                                  <path d="M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1"></path>
+                                </svg>
+                                <span>{copied ? "Copied!" : "Copy Code"}</span>
+                              </button>
+                            </div>
+                          </CopyToClipboard>
+                          <div className="max-h-96 overflow-y-auto rounded-lg border border-gray-200">
+                            <LiveEditor
+                              className="font-mono text-sm p-4 min-w-fit"
+                              style={{ background: "#f8fafc" }}
+                              disabled
+                            />
+                          </div>
+                        </div>
+                      )}
+                    </LiveProvider>
+                  </div>
+                </div>
+              </React.Fragment>
+            );
+          })}
       </div>
     </>
   );
